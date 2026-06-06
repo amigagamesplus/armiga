cd5457d18d4475b6d9cb2550ed90ea8a32d430c6
amigagamesplus <vqr2000@hotmail.com>
2026-06-06 19:33:49 +0200

    post-build: sobreescribir os-release con nombre armiga 1.0

--- board/armiga/post-build.sh
+++ board/armiga/post-build.sh
@@ -48,3 +48,13 @@ fi
 KVER="7.0.2-armiga"
 depmod -a -b "$TARGET_DIR" "$KVER" || true
 echo ">>> Armiga post-build.sh: done"
+
+# --- OS name -----------------------------------------------------------------
+cat > "$TARGET_DIR/etc/os-release" << 'OS_EOF'
+NAME="armiga"
+VERSION="1.0"
+ID=armiga
+VERSION_ID=1.0
+PRETTY_NAME="armiga 1.0"
+OS_EOF
+ln -sf ../etc/os-release "$TARGET_DIR/usr/lib/os-release" 2>/dev/null || true
