d74d6f03a7ba027de4875f29216c51f65ead5824
amigagamesplus <vqr2000@hotmail.com>
2026-07-05 11:09:55 +0200

    launcher: fix mapeo BTN_SDL_A/B - B=0 fisico, A=1 fisico + fix SHA256 antes de verificar

--- package/armiga-launcher/src/main.c
+++ package/armiga-launcher/src/main.c
@@ -94,8 +94,8 @@ static const char *DEV_MENU_ITEMS[] = {
 #define DEV_MENU_COUNT 4
 
 /* SDL button indices del H700 (confirmados en hardware, no kernel/evdev) */
-#define BTN_SDL_B      1
-#define BTN_SDL_A      0
+#define BTN_SDL_B      0
+#define BTN_SDL_A      1
 #define BTN_SDL_L1     4
 #define BTN_SDL_R1     5
 #define BTN_SDL_SELECT 8
