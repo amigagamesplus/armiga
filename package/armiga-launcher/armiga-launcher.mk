0e699c390c207ce5fb8392c1f16c36707e438c9d
amigagamesplus <vqr2000@hotmail.com>
2026-09-12 20:50:45 +0200

    launcher: remove unused -lm from LDFLAGS (log10f volume formula replaced by fixed gain, no libm functions used)

--- package/armiga-launcher/armiga-launcher.mk
+++ package/armiga-launcher/armiga-launcher.mk
@@ -10,7 +10,7 @@ ARMIGA_LAUNCHER_DEPENDENCIES = sdl3 sdl3_ttf sdl3_image
 define ARMIGA_LAUNCHER_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" \
 		CFLAGS="$(TARGET_CFLAGS) -flto -I$(TARGET_DIR)/usr/include" \
-		LDFLAGS="$(TARGET_LDFLAGS) -flto -L$(STAGING_DIR)/usr/lib -lSDL3 -lSDL3_ttf -lSDL3_image -lm" \
+		LDFLAGS="$(TARGET_LDFLAGS) -flto -L$(STAGING_DIR)/usr/lib -lSDL3 -lSDL3_ttf -lSDL3_image" \
 		-C $(@D)
 endef
 define ARMIGA_LAUNCHER_INSTALL_TARGET_CMDS
